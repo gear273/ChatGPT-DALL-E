6c98aa13e7e4f4bcd00eea210e91b1e88f78b3c4
Caleb Hrenchir <caleb@calebhrenchir.com>
2023-06-12 15:53:50 -0700

    Initial Commit

---
+++ ChatGPT-DALLE/ChatGPT_DALLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_DALLEApp.swift
+//  ChatGPT-DALLE
+//
+//  Created by Caleb Hrenchir on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_DALLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPT-DALLE/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatGPT-DALLE
+//
+//  Created by Caleb Hrenchir on 6/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
